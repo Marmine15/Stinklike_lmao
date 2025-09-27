169b134f0823c209cb6f85628c5c937a824e2b99
Benjamin <bengscribner@gmail.com>
2025-09-27 11:22:30 +0200

    added guns function

---
+++ Assets/Scripts/Player/ObjectPooler.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectPooler : MonoBehaviour
+{
+    public static ObjectPooler instance;
+
+    private List<GameObject> pooledObjects = new List<GameObject>();
+    public int amountToPool = 20;
+
+    [SerializeField] private GameObject bulletPrefab;
+
+    private void Awake() 
+    {
+        if (instance == null) 
+        {
+            instance = this;
+        }
+    }
+
+    // Use this for initialisation
+    void Start () 
+    {
+        for (int i = 0; i < amountToPool; i++)
+        {
+            GameObject obj = Instantiate(bulletPrefab);
+            obj.SetActive(false);
+            pooledObjects.Add(obj); 
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+        return null;
+    }
+}
